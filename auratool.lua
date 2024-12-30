1c8c3718c7e20feb657e92d188f3ea4c0dafe467
Gấu Kẹo <maicutewa2007@gmail.com>
2024-12-30 21:44:07 +0700

    Update auratool.lua

--- auratool.lua
+++ auratool.lua
@@ -4,13 +4,15 @@ getgenv().settings = {
     Aimbot_FOV_Radius = 100,
     -- màu vòng tròn ngoài
     Aimbot_FOV_Color = Color3.fromRGB(255, 255, 0),
-    -- màu tâm +
+    -- màu tâm
     FOV_CrossColor = Color3.fromRGB(255, 0, 0),
+    -- độ dày của tâm
+    FOV_CrossSize = 10,
     -- tầm đánh tối đa
     MaxDistance = 900,
     -- tốc độ đánh chỉnh 0 cũng được
     -- địch die rất nhanh cả mana của you
-    AuraLoopDelay = 0.1,
+    AuraLoopDelay = 0,
     -- chế độ tấn công
     -- 1 Player&Npc
     -- 2 Chỉ Player
